cc7f8bd2e0cefd7b3ad9453ce3b62aadd1eafbe9
Thejas0604 <meddepolamact.21@uom.lk>
2024-06-24 18:53:32 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import CreatePost from './components/Posts/CreatePost'
+
+function App() {
+  return (
+    <div><CreatePost></CreatePost></div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Posts/CreatePost.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const CreatePost = () => {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+    },
+    validationSchema: Yup.object({
+      title: Yup.string().required("Title is required"),
+      description: Yup.string().required("Description is required"),
+    }), //cannot submit without filling these fields
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          {...formik.getFieldProps("title")}
+        />
+        {formik.touched.title && formik.errors.title && (
+          <span>{formik.errors.title}</span>
+        )}
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          {...formik.getFieldProps("description")}
+        />
+        {formik.touched.description && formik.errors.description && (
+          <span>{formik.errors.description}</span>
+        )}
+        <button type="submit">Create Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
