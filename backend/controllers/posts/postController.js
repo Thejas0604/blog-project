55a2b92def06f34be65fbbdea9e3a0815f9a2595
Thejas0604 <meddepolamact.21@uom.lk>
2024-07-03 20:48:28 +0530

    post controllers

---
+++ backend/controllers/posts/postController.js
@@ -0,0 +1,80 @@
+const asyncHandler = require("express-async-handler");
+const Post = require("../../models/Post/Post");
+
+postController = {
+  //create post
+  createPost: asyncHandler(async (req, res) => {
+    const postData = req.body;
+    //console.log({title, content});
+    const postFound = await Post.findOne({ title: postData.title });
+    if (postFound) {
+      throw new Error("Post already exists");
+    }
+    const newPost = await Post.create(postData);
+    res.status(201).json({
+      status: "success",
+      message: "Post created successfully",
+      newPost,
+    });
+  }),
+  //get all posts
+  getAllPosts: asyncHandler(async (req, res) => {
+    const posts = await Post.find();
+    res.status(200).json({
+      status: "success",
+      posts,
+    });
+  }),
+  //get post by id
+  getPostById: asyncHandler(async (req, res) => {
+    const postId = req.params.postId;
+    const postFound = await Post.findById(postId);
+    if (!postFound) {
+      throw new Error("Post not found");
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Post found",
+      postFound,
+    });
+  }),
+  //update post
+  updatePost: asyncHandler(async (req, res) => {
+    //console.log(req.params);
+    const postId = req.params.postId;
+    const postFound = await Post.findById(postId);
+    if (!postFound) {
+      throw new Error("Post not found");
+    }
+    const postUpdated = await Post.findByIdAndUpdate(
+      postId,
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({
+      status: "success",
+      message: "Post updated successfully",
+      postUpdated,
+    });
+  }),
+  //delete post
+  deletePost: asyncHandler(async (req, res) => {
+    const postId = req.params.postId;
+    const deletedPost = await Post.findByIdAndDelete(postId);
+    if (!deletedPost) {
+      throw new Error("Post not found");
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Post deleted successfully",
+      deletedPost,
+    });
+  })
+};
+
+module.exports = postController;
--- backend/routers/post/postRouter.js
+++ backend/routers/post/postRouter.js
@@ -1,94 +1,20 @@
 const express = require("express");
 const postRouter = express.Router();
-const asyncHandler = require("express-async-handler");
-const Post = require("../../models/Post/Post");
+const postController = require("../../controllers/posts/postController");
 
 //Create post
-postRouter.post(
-  "/api/v1/posts/create",
-  asyncHandler(async (req, res) => {
-    const postData = req.body;
-    //console.log({title, content});
-    const postFound = await Post.findOne({ title: postData.title });
-    if (postFound) {
-      throw new Error("Post already exists");
-    }
-    const newPost = await Post.create(postData);
-    res.status(201).json({
-      status: "success",
-      message: "Post created successfully",
-      newPost,
-    });
-  })
-);
+postRouter.post("/api/v1/posts/create", postController.createPost);
 
 //get all posts
-postRouter.get(
-  "/api/v1/posts",
-  asyncHandler(async (req, res) => {
-    const posts = await Post.find();
-    res.status(200).json({
-      status: "success",
-      posts,
-    });
-  })
-);
+postRouter.get("/api/v1/posts", postController.getAllPosts);
 
 //get post by id
-postRouter.get(
-  "/api/v1/posts/:postId",
-  asyncHandler(async (req, res) => {
-    const postId = req.params.postId;
-    const postFound = await Post.findById(postId);
-    res.status(200).json({
-      status: "success",
-      message: "Post found",
-      postFound,
-    });
-  })
-);
+postRouter.get("/api/v1/posts/:postId", postController.getPostById);
 
 //update post
-postRouter.put(
-  "/api/v1/posts/:postId",
-  asyncHandler(async (req, res) => {
-    //console.log(req.params);
-    const postId = req.params.postId;
-    const postFound = await Post.findById(postId);
-    if (!postFound) {
-      throw new Error("Post not found");
-    }
-    const postUpdated = await Post.findByIdAndUpdate(
-      postId,
-      {
-        title: req.body.title,
-        content: req.body.content,
-      },
-      {
-        new: true,
-      }
-    );
-    res.status(200).json({
-      status: "success",
-      message: "Post updated successfully",
-      postUpdated,
-    });
-  })
-);
+postRouter.put("/api/v1/posts/:postId", postController.updatePost);
 
 //delete post
-postRouter.delete(
-    "/api/v1/posts/:postId",
-    asyncHandler(async (req, res) => {
-      const postId = req.params.postId;
-      const deletedPost = await Post.findByIdAndDelete(postId);
-      res.status(200).json({
-        status: "success",
-        message: "Post deleted successfully",
-        deletedPost,
-      });
-    })
-  );
-  
+postRouter.delete("/api/v1/posts/:postId", postController.deletePost);
 
 module.exports = postRouter;
