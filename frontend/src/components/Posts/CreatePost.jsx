ee26246a18372abfa404fd6cc5e8c2c66bf6f269
Thejas0604 <meddepolamact.21@uom.lk>
2024-07-04 19:47:51 +0530

    small tailwind

--- frontend/src/components/Posts/CreatePost.jsx
+++ frontend/src/components/Posts/CreatePost.jsx
@@ -34,7 +34,7 @@ const CreatePost = () => {
   const isError = postMutation.isError;
   const error = postMutation.error;
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center bg-gray-200">
       <div className="max-w-md w-full bg-white rounded-lg shadow-md p-8 m-4">
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-8">
           Add New Post
@@ -51,12 +51,23 @@ const CreatePost = () => {
           {formik.touched.description && formik.errors.description && (
             <span>{formik.errors.description}</span>
           )}
-          <button
-            className="p-2 text-white bg-purple-500 hover:bg-pink-600 rounded-2xl"
-            type="submit"
-          >
-            Create Post
-          </button>
+          <div>Category</div>
+          <div className="bg-gray-100 p-4 ">
+            <div className="text-center">Upload Image</div>
+            <div className="flex justify-center mt-2">
+              <button className="p-2 text-white bg-blue-500 rounded-2xl">
+                Choose a file
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-center mt-2">
+            <button
+              className="p-2 text-white bg-purple-500 hover:bg-pink-600 rounded-2xl"
+              type="submit"
+            >
+              Create Post
+            </button>
+          </div>
           {isLoading && <div>Creating the post...</div>}
           {isSuccess && <div>Post created successfully</div>}
           {isError && <div>{error.response.data.message} </div>}
